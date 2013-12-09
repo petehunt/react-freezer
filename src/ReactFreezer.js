929f2286a99361d15d9a3dc63cccc0dbe76094fa
petehunt <floydophone@gmail.com>
2013-12-08 17:10:47 -0800

    First pass at ReactFreezer

---
+++ src/ReactFreezer.js
@@ -0,0 +1,68 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var debounce = require('debounce');
+
+var LOCAL_STORAGE_DEBOUNCE = 200;
+
+var LocalStorageFreezer = {
+  write: debounce(
+    function(key, value) {
+      window.localStorage.setItem('react_freezer_' + key, JSON.stringify(value));
+    },
+    LOCAL_STORAGE_DEBOUNCE
+  ),
+
+  read: function(key) {
+    return JSON.parse(window.localStorage.getItem('react_freezer_' + key));
+  }
+};
+
+function getKeyForComponent(component) {
+  var displayName = component.constructor.displayName || 'ReactCompositeComponent';
+  var depth = component._mountDepth;
+  var nodeID = component._rootNodeID;
+
+  return displayName + ',' + depth + ',' + nodeID;
+}
+
+var ReactFreezerMixin = {
+  componentDidUpdate: function() {
+    this.freezer.write(getKeyForComponent(this), this.state);
+  }
+};
+
+function createFreezableClass(spec) {
+  if (!spec.getInitialState) {
+    throw new Error(
+      spec.displayName + ': Creating a freezable class on a component without state doesn\'t make sense.'
+    );
+  }
+
+  if (!spec.freezer) {
+    throw new Error(
+      spec.displayName + ': Cannot create a freezable class without a freezer!'
+    );
+  }
+
+  var originalGetInitialState = spec.getInitialState;
+
+  spec.getInitialState = function() {
+    var state = spec.freezer.read(getKeyForComponent(this));
+    if (!state) {
+      return originalGetInitialState.call(this);
+    }
+  };
+
+  return React.createClass(spec);
+}
+
+var ReactFreezer = {
+  Mixin: ReactFreezerMixin,
+  Freezers: {
+    LocalStorage: LocalStorageFreezer
+  }
+};
+
+module.exports = ReactFreezer;
